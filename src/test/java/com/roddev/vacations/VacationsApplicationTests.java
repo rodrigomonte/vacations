a5ba3803c5d64085034195e792d52e42e40a7420
Rodrigo <rodrigo.monte86@gmail.com>
2021-09-20 19:48:37 -0400

    Project from Spring Initializr as-is

---
+++ src/test/java/com/roddev/vacations/VacationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roddev.vacations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
